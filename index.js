c6ae0df6c79a27a0dd0831f52fe205860653527c
William Soukkachang <WSoukkachang@yahoo.com>
2019-06-15 12:04:46 -0500

    Added CRUD methods and answered questions.

--- index.js
+++ index.js
@@ -12,8 +12,53 @@ server.use(express.json());
 
 // END POINTS //
 
+// GET ALL PROJECTS
 
-const port = process.env.PORT || 7000;
+server.get('/api/projects', async (req, res) => {
+  try {
+    const projects = await db('projects'); // all the projects from the table
+    res.status(200).json(projects);
+  } catch {
+    res.status(500).json({error: "Error retrieving data"})
+  }
+})
+
+// POST PROJECTS
+
+server.post('/api/projects', async (req, res) => {
+  try {
+    const [ id ] = await db('projects').insert(req.body)
+    const project = await db('projects').where({ id }).first();
+    res.status(201).json(project);
+  } catch {
+    res.status(500).json({error: "Error posting data"})
+  }
+})
+
+// GET ALL ACTIONS
+
+server.get('/api/actions', async (req, res) => {
+  try {
+    const actions = await db('actions'); // all the projects from the table
+    res.status(200).json(actions);
+  } catch {
+    res.status(500).json({error: "Error retrieving data"})
+  }
+})
+
+// POST ACTIONS
+
+server.post('/api/actions', async (req, res) => {
+  try {
+    const [ id ] = await db('actions').insert(req.body)
+    const action = await db('actions').where({ id }).first();
+    res.status(201).json(action);
+  } catch {
+    res.status(500).json({error: "Error posting data"})
+  }
+})
+
+const port = process.env.PORT ||  6000;
 server.listen(port, () =>
   console.log(`\n** API running on http://localhost:${port} **\n`)
 );
